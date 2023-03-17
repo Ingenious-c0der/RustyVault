ee6a90ee113855f2f07bc19a5c954bb43e7ef9c6
INGENIOUS <sagarabhyankar18@gmail.com>
2023-03-17 23:49:50 +0530

    -key table

--- src-tauri/src/db/models.rs
+++ src-tauri/src/db/models.rs
@@ -1,4 +1,5 @@
 use crate::schema::pst; 
+use crate::schema::key_table;
 use serde:: {Deserialize, Serialize};
 
 #[derive(Queryable, Serialize, Deserialize, Debug)]
@@ -13,6 +14,24 @@ pub struct Pst {
 pub struct NewPst<'a>{
     pub name: &'a str,
     pub password: &'a str,
+}
+
+
+
+#[derive(Queryable, Serialize, Deserialize, Debug)]
+pub struct KeyTable{
+    pub id: i32,
+    pub name: String,
+    pub key: String,
+    pub user_id: i32,
+}
+
+#[derive(Insertable, Serialize, Deserialize, Debug)]
+#[table_name = "key_table"]
+pub struct NewKeyTable<'a>{
+    pub name: &'a str,
+    pub key: &'a str,
+    pub user_id: i32,
 }
 
 
--- src-tauri/src/schema.rs
+++ src-tauri/src/schema.rs
@@ -5,3 +5,12 @@ table! {
         password -> Text,
     }
 }
+table! {
+    key_table(id)
+    {
+        id -> Integer,
+        name -> Text,
+        key -> Text,
+        user_id -> Integer,
+    }
+}
