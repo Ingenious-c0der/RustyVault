d6751cfe27ce5d9120ffebcf31e85f551bbb8585
INGENIOUS <sagarabhyankar18@gmail.com>
2023-03-20 20:46:00 +0530

    -minor

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,30 +1,76 @@
-import React from 'react';
-import {Card,Button} from 'react-bootstrap';
+import React from "react";
+import { Card, Button } from "react-bootstrap";
 //import font awesome icons
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { MdVisibility, MdContentPaste } from "react-icons/md";
 class HomePage extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-
-      }
-    }
-    render() {
-      return (
-        <div className="container">
-        <Card  style={{ overflow:true,opacity:0.9,backgroundColor:"",padding: "10px", width: '15rem' ,border: "solid 3px #540f00" ,borderRadius:"30px"}}>
-          <Card.Img variant="top" style = {{borderRadius:"10px" }}src="https://t3.ftcdn.net/jpg/03/86/50/54/240_F_386505487_omkU0kGEhMa3gQ83rVksoXX41AFFfi0K.jpg" width = "100px"/>
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="container">
+        <Card
+          style={{
+            overflow: true,
+            opacity: 0.9,
+            backgroundColor: "",
+            padding: "10px",
+            width: "10rem",
+            border: "solid 3px #540f00",
+            borderRadius: "30px",
+          }}
+        >
+          <Card.Img
+            variant="top"
+            style={{ backgroundColor:"Red",borderRadius: "10px" }}
+            src="https://t3.ftcdn.net/jpg/03/86/50/54/240_F_386505487_omkU0kGEhMa3gQ83rVksoXX41AFFfi0K.jpg"
+            width="80px"
+          />
           <Card.Body>
-
-            <div>
-              <Button>Copy to Clipboard</Button>
-            </div>
-            <Button >Reveal</Button>
+            <row>
+            <Button title ="Copy Password to Clipboard" style = {{marginRight: "16px"}}>
+              <MdContentPaste />
+            </Button>
+           
+            <Button title ="Reveal Password">
+              <MdVisibility />
+            </Button>
+            </row>
           </Card.Body>
         </Card>
-        </div>
-      );
-      }
+        <Card
+          style={{
+            overflow: true,
+            opacity: 0.9,
+            backgroundColor: "",
+            padding: "10px",
+            width: "10rem",
+            border: "solid 3px #540f00",
+            borderRadius: "30px",
+          }}
+        >
+          <Card.Img
+            variant="top"
+            style={{ backgroundColor:"Red",borderRadius: "10px" }}
+            src="https://t3.ftcdn.net/jpg/03/86/50/54/240_F_386505487_omkU0kGEhMa3gQ83rVksoXX41AFFfi0K.jpg"
+            width="80px"
+          />
+          <Card.Body>
+            <row>
+            <Button title ="Copy Password to Clipboard" style = {{marginRight: "16px"}}>
+              <MdContentPaste />
+            </Button>
+           
+            <Button title ="Reveal Password">
+              <MdVisibility />
+            </Button>
+            </row>
+          </Card.Body>
+        </Card>
+      </div>
+    );
+  }
 }
 export default HomePage;
